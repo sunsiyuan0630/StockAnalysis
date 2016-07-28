020971c226389e0ade1789c09a4a1fdfdc10224f
sunsiyuan0630 <609848129@qq.com>
2016-07-28 14:39:01 +0800

    fix

--- ImportStaticData.py
+++ ImportStaticData.py
@@ -4,6 +4,14 @@ import pymongo
 import json
 
 
+def insertIndex(dict):
+    list = []
+    for k, v in dict.items():
+        v['code'] = k
+        list.append(v)
+    return list
+
+
 clinet = pymongo.MongoClient("localhost", 27017)
 
 db = clinet["Stock"]
@@ -11,21 +19,16 @@ basics = db["Basics"]
 industry = db['Industry']
 concept = db['Concept']
 
+
 basicDf = ts.get_stock_basics()
-basicDict = json.loads(basicDf.to_json(orient='records'),encoding="UTF-8")
-basics.insert(basicDict)
+basicDict = json.loads(basicDf.to_json(orient='index'), encoding="UTF-8")
+basicList = insertIndex(basicDict)
+basics.insert(basicList)
 
 industryDf = ts.get_industry_classified()
-industryDict = json.loads(industryDf.to_json(orient='records'),encoding="UTF-8")
+industryDict = json.loads(industryDf.to_json(orient='records'), encoding="UTF-8")
 industry.insert(industryDict)
 
 conceptDf = ts.get_concept_classified()
-conceptDict = json.loads(conceptDf.to_json(orient='records'),encoding="UTF-8")
+conceptDict = json.loads(conceptDf.to_json(orient='records'), encoding="UTF-8")
 concept.insert(conceptDict)
-
-
-
-
-
-
-
